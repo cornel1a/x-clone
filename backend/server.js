a5cb05a2047ac5c24f4ebc5901ef3539f981dc61
cornel1a <corneliayue@gmail.com>
2025-05-08 16:06:02 -0400

    4- fix wildcard due to express v5 upgrade

--- backend/server.js
+++ backend/server.js
@@ -45,9 +45,9 @@ app.use("/api/notifications", notificationRoutes);
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
+	app.get("/*path", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+      });
 }
 
 app.listen(PORT, () => {
